012139b652f80c334ca3bfd6019bba713bff1367
Jonas Vercammen2 <jonas.vercammen@vente-exclusive.com>
2019-12-18 23:45:48 +1300

    feat(blog):add-local-blogpost:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'blog';
+  title = "blog";
+
+  public blogs = this.http.get("assets/blog-posts.json");
+
+  constructor(private http: HttpClient) {}
 }
