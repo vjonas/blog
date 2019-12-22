8ba26c01d3e4f02f1c9913ea8e1d3f1f12f67bd8
Jonas Vercammen2 <jonas.vercammen@vente-exclusive.com>
2019-12-22 17:40:45 +1300

    feat(ios): add scrolling issues for ios + remove background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { HttpClient } from "@angular/common/http";
-import { Component } from "@angular/core";
+import { Component, OnInit, HostListener } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @HostListener("window:resize") resizing = () => this.resize();
   title = "blog";
 
   public blogs$ = this.http.get("assets/blog-posts.json");
 
   constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  private resize = () => {
+    let vh = window.innerHeight * 0.01;
+    document
+      .querySelector(".background")[0]
+      .style.setProperty("--vh", `${vh}px`);
+  };
 }
