4f683fc8119916311cfe4035f67603337275f0ca
Jonas Vercammen2 <jonas.vercammen@vente-exclusive.com>
2019-12-27 00:22:15 +1300

    fix

--- server/auth.js
+++ server/auth.js
@@ -4,11 +4,13 @@ const fs = require("fs");
 //   .toLocaleString();
 
 exports.myAuth = function(header) {
-  console.log("LOGGED", header, `heaer:${header.auth}`);
+  // console.log("LOGGED", header, `heaer:${header.auth}`);
   if (process.env.NODE_ENV) {
-    const passwd = fs.readFileSync(
-      "/home/jonas/git/https-server-blogs/password"
-    );
+    const passwd = fs
+      .readFileSync("/home/jonas/git/https-server-blogs/password")
+      .toLocaleString();
+    console.log("file", passwd, "auth", header.auth);
+
     if (passwd === header.auth) {
       return true;
     }
