be4e16a9b702d1ce876706488032b189eb18b417
Jonas Vercammen2 <jonas.vercammen@vente-exclusive.com>
2019-12-27 00:32:00 +1300

    fix-auth

--- server/auth.js
+++ server/auth.js
@@ -1,17 +1,12 @@
 const fs = require("fs");
-// const passwd = fs
-//   .readFileSync("/Users/jonasvercammen/password")
-//   .toLocaleString();
 
 exports.myAuth = function(header) {
-  // console.log("LOGGED", header, `heaer:${header.auth}`);
   if (process.env.NODE_ENV) {
     const passwd = fs
       .readFileSync("/home/jonas/git/https-server-blogs/password")
       .toLocaleString();
-    console.log("file", passwd, "auth", header.auth);
 
-    if (passwd === header.auth) {
+    if (passwd.toString().trim() === header.auth.toString().trim()) {
       return true;
     }
     return false;
