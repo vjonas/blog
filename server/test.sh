34cfda3768738b77d15a8eb5a1ed54391bd08bfd
Jonas Vercammen2 <jonas.vercammen@vente-exclusive.com>
2019-12-29 11:50:11 +1300

    fix prod build

--- server/test.sh
+++ server/test.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
-echo start hook;
-cd ~/git/blog;
-git pull;
+echo start hook
+cd ~/git/blog
+git pull
 echo running npm install
-npm install;
+npm install
 echo building angular
-ng build --prod
+ng build --prod &
 echo build done
 lsof -i tcp:3000 -t | xargs kill -9
 NODE_ENV=production node ./server/index.js &
