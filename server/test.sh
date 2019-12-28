25b7354abf81a33d0d461adacf3072f9e238ea33
Jonas Vercammen2 <jonas.vercammen@vente-exclusive.com>
2019-12-28 23:17:11 +1300

    update-server-script

--- server/test.sh
+++ server/test.sh
@@ -5,8 +5,8 @@ git pull;
 echo running npm install
 npm install;
 echo building angular
-ng build --prod;
-echo build done;
-killproc;
-NODE_ENV=production node ./server/index.js &;
-echo started node server;
+ng build --prod
+echo build done
+lsof -i tcp:3000 -t | xargs kill -9
+NODE_ENV=production node ./server/index.js &
+echo started node server
